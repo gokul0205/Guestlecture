51e919873c7ae2de78c36d558629a878636ac181
Vikram <vikram.c1999@gmail.com>
2020-03-14 19:20:31 +0530

    Check THIS!

---
+++ app/src/main/java/com/example/guestlec/CheckChange.java
@@ -0,0 +1,14 @@
+package com.example.guestlec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckChange extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_change);
+    }
+}
